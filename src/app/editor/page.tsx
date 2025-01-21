464b4beab74ba0e6bfd144a5ab7056e3bd8e4438
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-01-21 11:58:25 +0100

    Fix: typescript and deprecated tldraw functions

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,38 +1,44 @@
 "use client";
 
-import { Tldraw, TldrawEditor, Editor } from "@tldraw/tldraw";
+import { Tldraw, Editor, getSnapshot, loadSnapshot } from "tldraw";
 import { trpc } from "@/utils/trpc";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import debounce from "lodash.debounce";
 import "tldraw/tldraw.css";
 
 export default function EditorPage() {
-  const { data } = trpc.getData.useQuery();
+  const { data: initialData } = trpc.getData.useQuery();
   const saveData = trpc.saveData.useMutation();
-  const [editor, setEditor] = useState<TldrawEditor | null>(null);
+  const [editor, setEditor] = useState<Editor | null>(null);
 
   const debouncedSave = useCallback(
-    debounce((editorInstance: typeof TldrawEditor) => {
-      const snapshot = editorInstance.store.getSnapshot();
+    debounce((editorInstance: Editor) => {
+      const snapshot = getSnapshot(editorInstance.store);
       saveData.mutate({ data: snapshot });
     }, 1000),
     [saveData]
   );
 
   const handleMount = useCallback(
-    (editorInstance: TldrawEditor) => {
+    (editorInstance: Editor) => {
       setEditor(editorInstance);
 
-      if (data) {
-        editorInstance.store.loadSnapshot(data);
+      if (initialData) {
+        loadSnapshot(editorInstance.store, initialData);
       }
 
       editorInstance.store.listen(() => {
         debouncedSave(editorInstance);
       });
     },
-    [data, debouncedSave]
+    [initialData, debouncedSave]
   );
+
+  useEffect(() => {
+    if (editor && initialData) {
+      loadSnapshot(editor.store, initialData);
+    }
+  }, [editor, initialData]);
 
   return (
     <div className='grid grid-rows-12 h-screen'>
@@ -41,7 +47,8 @@ export default function EditorPage() {
           Modify Shape
         </button>
       </div>
-      <div className='row-span-11 border-4 border-black p-4rounded-lg'>
+
+      <div className='row-span-11 border-4 border-black p-4 rounded-lg'>
         <Tldraw onMount={handleMount} className='rounded-lg' />
       </div>
     </div>
